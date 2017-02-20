8bec6c8670c9aaecbc0b5ab7d5eb7ae0c37164f2
Gilberto Albino <zyneframework@zyne.com.br>
2017-02-19 21:06:32 -0300

    Files committed

---
+++ src/Contract/AbstractView.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Zyne Framework (https://phpframework.zyne.com.br/)
+ *
+ * @link      https://github.com/zynebr/phpframework para acesso ao repositório
+ * @copyright Copyright (c) 2007-2017 Zyne BR. (https://www.zyne.com.br)
+ * @license   https://framework.zyne.com.br/licensa/new-bsd New BSD License
+ *
+ */
+
+namespace Zyne\Contract;
+
+use Zyne\Core\FactoriableTrait;
+
+/**
+ * Class AbstractView
+ * @package Zyne\Contract
+ */
+abstract class AbstractView implements ViewInterface
+{
+    use FactoriableTrait;
+}
