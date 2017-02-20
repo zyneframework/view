cc4435c67fdee8380ddc7ecff7310ccc363ae888
Gilberto Albino <zyneframework@zyne.com.br>
2017-02-20 20:16:17 -0300

    Files committed

---
+++ src/bin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Zyne Framework (https://phpframework.zyne.com.br/)
+ *
+ * @link      https://github.com/zynebr/phpframework para acesso ao repositório
+ * @copyright Copyright (c) 2007-2017 Zyne BR. (https://www.zyne.com.br)
+ * @license   https://framework.zyne.com.br/licensa/new-bsd New BSD License
+ *
+ */
+
+/**
+ * Utilidades binárias do componente View
+ */
---
+++ src/helpers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Zyne Framework (https://phpframework.zyne.com.br/)
+ *
+ * @link      https://github.com/zynebr/phpframework para acesso ao repositório
+ * @copyright Copyright (c) 2007-2017 Zyne BR. (https://www.zyne.com.br)
+ * @license   https://framework.zyne.com.br/licensa/new-bsd New BSD License
+ *
+ */
+
+/**
+ * Utilidades Funcionais do Componente View
+ */
+if (!function_exists('view')) {
+    function view($viewScript)
+    {
+
+    }
+}
